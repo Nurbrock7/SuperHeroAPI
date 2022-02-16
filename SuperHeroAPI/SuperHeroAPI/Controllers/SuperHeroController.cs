352c6135f99e9bb47cced97bbf42ba6ca145524b
Moegamat-Nur Brock <Nurbrock7@gmail.com>
2022-02-16 17:31:37 +0200

    Added Put, Update and Delete Methods to Controller and added Entity Framework as well

--- SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -26,16 +26,18 @@ namespace SuperHeroAPI
             }
         };
 
-    [HttpGet]
-        public async Task<ActionResult<SuperHero>> Get()
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
         {
             return Ok(heros);
         }
-
+        [HttpGet("{id}")]
         public async Task<ActionResult<SuperHero>> Get(int id)
         {
-            var herp = heros.Find(h => h.Id == id);
-            return BadRequest("Hero Not Found");
+            var hero = heros.Find(h => h.Id == id);
+            if (heros == null)
+                return BadRequest("Hero Not Found");
+            return Ok(heros);
         }
         [HttpPost]
         public async Task<ActionResult<SuperHero>> AddHero(SuperHero hero)
@@ -43,5 +45,32 @@ namespace SuperHeroAPI
             heros.Add(hero);
             return Ok(heros);
         }
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
+        {
+            var hero = heros.Find(h => h.Id == request.Id);
+            if (heros == null)
+                return BadRequest("Hero Not Found");
+
+            hero.Name = request.Name;
+            hero.Place = request.Place;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.LastName = request.LastName;
+
+            return Ok(heros);
+
+        }
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        {
+            var hero = heros.Find(h => h.Id == id);
+            if (heros == null)
+                return BadRequest("Hero Not Found");
+
+            heros.Remove(hero);
+            return Ok(heros);
+        }
+
     }
 }
