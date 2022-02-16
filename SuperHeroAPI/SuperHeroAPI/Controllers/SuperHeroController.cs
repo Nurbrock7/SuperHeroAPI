36381b2f45ba41e87c745d710fdc05c1efb96048
Moegamat-Nur Brock <Nurbrock7@gmail.com>
2022-02-16 16:40:40 +0200

    Added new Controller with Get Method and added a list to it

---
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var heros = new List<SuperHero>
+            {
+                new SuperHero { Id = 1,
+                    FirstName = "Thor",
+                    LastName = "Ragnarok",
+                    Name =  "Chris",
+                    Place = "Azgard"}
+            };
+
+            return NotFound(heros);
+        }
+    }
+}
