082c651a151d5a01f2cbce4651bf68ae81d4ef76
Moegamat-Nur Brock <Nurbrock7@gmail.com>
2022-02-17 09:23:51 +0200

    Added new Db Context for SQL injection and Migrations to create table in SQL

--- SuperHeroAPI/SuperHeroAPI/Data/DataContext.cs
+++ SuperHeroAPI/SuperHeroAPI/Data/DataContext.cs
@@ -4,5 +4,8 @@ namespace SuperHeroAPI.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<SuperHero> SuperHero { get; set; }
     }
 }
